8c1dac24a334a455f26ade65c43d1f68e168fb72
jasonhillier <jason@paviasystems.com>
2016-08-22 17:46:13 -0700

    working, documentation

--- README.md
+++ README.md
@@ -2,4 +2,38 @@
 
 Command-Line utility to fetch and merge JSON data from Rancher metadata service
 
+## Install
+```sh
+$ npm install -g fetch-rancher-metadata
+```
+
+## Rancher-Compose.yml
+```yml
+myService:
+  # Scale of service
+  scale: 1
+  # User added metadata
+  metadata:
+    config:
+      option1: newValue1
+      option2: newValue2
+```
+
+## Docker container local JSON file (e.g. Application-Config.json)
+```json
+{
+    "option1": "oldValue",
+    "option2": "oldValue"
+}
+```
+
+## Execution within service docker container
 fetch-rancher-metadata --key "config" --merge "Application-Config.json"
+
+## Result (e.g. Application-Config.json)
+```json
+{
+    "option1": "newValue1",
+    "option2": "newValue2"
+}
+```
--- bin/fetch-rancher-metadata.js
+++ bin/fetch-rancher-metadata.js
@@ -7,12 +7,13 @@ var argv = require('yargs').argv;
 
 var _Key = argv.key ? argv.key : 'config';
 var _MergeFile = argv.merge ? argv.merge : 'Application-Config.json';
+var _WorkDir = process.cwd();
 
 libRequest({
     method: 'GET',
     url: 'http://rancher-metadata/latest/self/service/metadata',
     json: true,
-    timeout: 10
+    timeout: 2000
     }, function (err, pResponse)
     {
         if (err)
@@ -36,10 +37,9 @@ libRequest({
 
         //we want excpetions to be thrown so process exits with errorlevel and descriptive message,
         // so we might as well use sync procedures.
-        var tmpSource = JSON.parse(libFS.readFileSync(`${__dirname}/${_MergeFile}`));
-        var tmpTarget = libMerge(tmpSourceJSON, tmpMetadata[key]);
+        var tmpSource = JSON.parse(libFS.readFileSync(`${_WorkDir}/${_MergeFile}`));
+        var tmpTarget = libMerge(tmpSource, tmpMetadata[_Key]);
 
-        libFS.writeFileSync(`${__dirname}/${_MergeFile}`, JSON.stringify(tmpTarget));
-        console.trace('Updated JSON file:', _MergeFile);
-        process.exit(0);
+        libFS.writeFileSync(`${_WorkDir}/${_MergeFile}`, JSON.stringify(tmpTarget, null, 4));
+        console.log('Updated JSON file:', _MergeFile);
     });
